5960f9a1e3a68a562aa9811c36e112facd6d3a44
george <george@george.com>
2026-05-16 20:18:11 +0800

    feat: add AppState central state management + HermindClient dual API

--- desktop/src/HermindClient.h
+++ desktop/src/HermindClient.h
@@ -6,6 +6,7 @@
 #include <QNetworkReply>
 #include <QJsonObject>
 #include <QJSValue>
+#include <functional>
 
 class HermindClient : public QObject
 {
@@ -13,6 +14,18 @@ class HermindClient : public QObject
 public:
     explicit HermindClient(const QString &baseUrl, QObject *parent = nullptr);
 
+    using Callback = std::function<void(const QJsonObject &, const QString &error)>;
+
+    // C++ API (std::function callbacks)
+    void get(const QString &path, Callback callback);
+    void post(const QString &path, const QJsonObject &body, Callback callback);
+    void put(const QString &path, const QJsonObject &body, Callback callback);
+    void delete_(const QString &path, Callback callback);
+    void upload(const QString &path, const QByteArray &data,
+                const QString &fileName, const QString &mimeType,
+                Callback callback);
+
+    // QML API (QJSValue callbacks)
     Q_INVOKABLE void get(const QString &path, QJSValue callback);
     Q_INVOKABLE void post(const QString &path, QJsonObject body, QJSValue callback);
     Q_INVOKABLE void put(const QString &path, QJsonObject body, QJSValue callback);
@@ -21,6 +34,7 @@ public:
                             const QString &fileName, const QString &mimeType,
                             QJSValue callback);
     Q_INVOKABLE QNetworkReply* getStream(const QString &path);
+
     QString baseUrl() const;
 
 private:
